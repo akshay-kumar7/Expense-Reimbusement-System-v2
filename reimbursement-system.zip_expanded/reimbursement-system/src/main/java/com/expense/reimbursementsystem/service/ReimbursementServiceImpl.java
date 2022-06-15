89ab55b238b65722af4ce702cbd9cb810174a708
Fozeya-Nikka Alviar <fozeya-nikka397@revature.net>
2022-06-15 10:57:10 -0400

    cleaned code

--- reimbursement-system.zip_expanded/reimbursement-system/src/main/java/com/expense/reimbursementsystem/service/ReimbursementServiceImpl.java
+++ reimbursement-system.zip_expanded/reimbursement-system/src/main/java/com/expense/reimbursementsystem/service/ReimbursementServiceImpl.java
@@ -1,33 +1,8 @@
-package com.expense.reimbursementsystem.service;
+package com.expense.reimbursementsystem.service;	
 
-import org.springframework.beans.BeanUtils;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
-
-import com.expense.reimbursementsystem.dao.ReimbursementDao;
-import com.expense.reimbursementsystem.entity.ReimbursementEntity;
-import com.expense.reimbursementsystem.exception.ApplicationException;
-import com.expense.reimbursementsystem.pojo.ReimbursementPojo;
-
-@Service
-public class ReimbursementServiceImpl implements ReimbursementService{
-	
-	@Autowired
-	ReimbursementDao reimbursementDao;
-
-	@Override
-	public ReimbursementPojo submitInfo(ReimbursementPojo reimbursementPojo) throws ApplicationException {
-		// TODO Auto-generated method stub
-		
-		ReimbursementEntity reimbursementEntity = new ReimbursementEntity();
-		BeanUtils.copyProperties(reimbursementPojo, reimbursementEntity);
-		
-		ReimbursementEntity returnedReimbursementEntity = reimbursementDao.save(reimbursementEntity);
-		
-		return reimbursementPojo;
-	}
-	
-	
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -54,7 +29,6 @@ public class ReimbursementServiceImpl implements ReimbursementService{
 
 	@Override
 	public List<ReimbursementPojo> viewEmployeeRequests(int employeeId) throws ApplicationException {
-		
 		Optional<ReimbursementEntity> reimbursementEntityOpt = reimbursementDao.findById(employeeId);
 		List<ReimbursementPojo> reimbursementPojo = new ArrayList<ReimbursementPojo>();
 		if(reimbursementEntityOpt.isPresent()) {
@@ -88,6 +62,13 @@ public class ReimbursementServiceImpl implements ReimbursementService{
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
+
+	@Override
+	public ReimbursementPojo submitInfo(ReimbursementPojo reimbursementPojo) throws ApplicationException {
+		ReimbursementEntity reimbursementEntity = new ReimbursementEntity();
+		BeanUtils.copyProperties(reimbursementPojo, reimbursementEntity);
+		ReimbursementEntity returnedReimbursementEntity = reimbursementDao.save(reimbursementEntity);
+		return reimbursementPojo;
+	}
+
 }
